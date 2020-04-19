62f02dd81457be9ab823df30e5c9218b66aadd29
RuiHirano <r.hrn.0930@gmail.com>
2020-04-19 21:48:32 +0900

    [update]update kube util for uclab

---
+++ docker_push_uclab.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VERSION=$1
+echo "version is ${VERSION}"
+
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/nodeid-server:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/synerex-server:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/master-provider:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/worker-provider:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/agent-provider:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/visualization-provider:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/gateway-provider:${VERSION} 
+docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/simulator:${VERSION} 
+
+#docker image push ucl.nuee.nagoya-u.ac.jp/uclab/synerex-simulation/pod-manager:${VERSION} 
+
+echo "push finished"
