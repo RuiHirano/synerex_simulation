1f19f6e0817584233908a18550a88060e0d118eb
RuiHirano <r.hrn.0930@gmail.com>
2020-04-24 23:20:16 +0900

    [update]add com lp duration in agent-provider

--- provider/agent/agent-provider.go
+++ provider/agent/agent-provider.go
@@ -124,6 +124,7 @@ func (m *Message) Get() []*api.Agent {
 
 func forwardClock() {
 	//senderId := myProvider.Id
+	var com1, com2 int64
 	t1 := time.Now()
 	logger.Debug("1: 同エリアエージェント取得")
 	targets := pm.GetProviderIds([]simutil.IDType{
@@ -132,8 +133,11 @@ func forwardClock() {
 	sameAgents := []*api.Agent{}
 	if len(targets) != 0 {
 		senderId := myProvider.Id
+		comt1 := time.Now()
 		sps, _ := simapi.GetAgentRequest(senderId, targets)
-		logger.Debug("1: targets %v\n", targets)
+		comt2 := time.Now()
+		com1 = comt2.Sub(comt1).Milliseconds()
+		//logger.Debug("1: targets %v\n", targets)
 		//sps, _ := waiter.WaitSp(msgId, targets, 1000)
 		for _, sp := range sps {
 			agents := sp.GetSimSupply().GetGetAgentResponse().GetAgents()
@@ -144,7 +148,7 @@ func forwardClock() {
 	// [2. Calculation]次の時間のエージェントを計算する
 	logger.Debug("2: エージェント計算を行う")
 	nextControlAgents := sim.ForwardStep(sameAgents) // agents in control area
-	logger.Debug("2: Set")
+	//logger.Debug("2: Set")
 	agentsMessage.Set(nextControlAgents)
 
 	logger.Debug("3: 隣接エージェントを取得")
@@ -156,8 +160,11 @@ func forwardClock() {
 	neighborAgents := []*api.Agent{}
 	if len(targets) != 0 {
 		senderId := myProvider.Id
+		comt1 := time.Now()
 		sps, _ := simapi.GetAgentRequest(senderId, targets)
-		logger.Debug("3: targets %v\n", targets)
+		comt2 := time.Now()
+		com1 = comt2.Sub(comt1).Milliseconds()
+		//logger.Debug("3: targets %v\n", targets)
 		//sps, _ := waiter.WaitSp(msgId, targets, 1000)
 		for _, sp := range sps {
 			agents := sp.GetSimSupply().GetGetAgentResponse().GetAgents()
@@ -179,7 +186,9 @@ func forwardClock() {
 	logger.Info("Finish: Clock Forwarded. AgentNum:  %v", len(nextControlAgents))
 	t2 := time.Now()
 	duration := t2.Sub(t1).Milliseconds()
-	logger.Info("Duration: %v, PID: %v", duration, myProvider.Id)
+	comDuration := com2 - com1
+	lpDuration := duration-comDuration
+	logger.Info("Total: %v, ComDuration: %v, LpDuration: %v", duration, comDuration, lpDuration)
 }
 
 // callback for each Supply
